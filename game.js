ff2f146007480d1852189d4b52d80aceb458af80
Vaibhav Tiwari <vaibhav.tiwari210@gmail.com>
2023-10-31 15:17:55 +0530

    deploy

---
+++ game.js
@@ -0,0 +1,8 @@
+const question = document.getElementById("question");
+const choices = Array.from(document.getElementsByClassName("choice-text"));
+console.log(choices);
+
+let currentQuestion = {};
+let acceptingAnswers = true;
+let score = 0;
+let availableQuestions = [];
